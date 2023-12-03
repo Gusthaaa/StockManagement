149ef4a16ad56f1f80fce08609c52dff9de9c278
Gustavo <gustavohatanaka@edu.unifil.br>
2023-12-02 22:19:52 -0300

    Correção 2.0

---
+++ StockSystem/backend/models/Product.js
@@ -0,0 +1,86 @@
+import db from "../config/database.js";
+
+export const getProducts = (result) => {
+  db.query("SELECT * FROM products", (err, results) => {
+    if (err) {
+      console.log(err);
+      result(err, null);
+    } else {
+      result(null, results);
+    }
+  });
+};
+
+export const getProductById = (id, result) => {
+  db.query("SELECT * FROM products WHERE id = ?", [id], (err, results) => {
+    if (err) {
+      console.log(err);
+      result(err, null);
+    } else {
+      result(null, results[0]);
+    }
+  });
+};
+
+export const insertProduct = (data, result) => {
+  db.query("INSERT INTO products SET ?", [data], (err, results) => {
+    if (err) {
+      console.log(err);
+      result(err, null);
+    } else {
+      result(null, results);
+    }
+  });
+};
+
+export const updateProductById = (data, id, result) => {
+  db.query("UPDATE products SET ? WHERE id = ?", [data, id], (err, results) => {
+    if (err) {
+      console.log(err);
+      result(err, null);
+    } else {
+      result(null, results);
+    }
+  });
+};
+
+export const updateProductStatus = (id, status, result) => {
+  db.query(
+    "UPDATE products SET status = ? WHERE id = ?",
+    [status, id],
+    (err, results) => {
+      if (err) {
+        console.log(err);
+        result(err, null);
+      } else {
+        result(null, results);
+      }
+    }
+  );
+};
+
+export const updateProductQuantity = (id, quantity, result) => {
+  db.query(
+    "UPDATE products SET quantity = ? WHERE id = ?",
+    [quantity, id],
+    (err, results) => {
+      if (err) {
+        console.log(err);
+        result(err, null);
+      } else {
+        result(null, results);
+      }
+    }
+  );
+};
+
+export const deleteProductById = (id, result) => {
+  db.query("DELETE FROM products WHERE id = ?", [id], (err, results) => {
+    if (err) {
+      console.log(err);
+      result(err, null);
+    } else {
+      result(null, results);
+    }
+  });
+};
---
+++ StockSystem/backend/routes/routes.js
@@ -0,0 +1,80 @@
+// import express
+import express from "express";
+
+// import function from controller
+import {
+  showProducts,
+  showProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  updateProductStatusController,
+  updateProductQuantityController,
+} from "../controllers/ProductController.js";
+
+import {
+  showCategories,
+  showCategoryById,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+} from "../controllers/CategoryController.js";
+
+import {
+  showUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+  showUserByUsername,
+  updateUserStatusController,
+} from "../controllers/UserController.js";
+
+import { loginUser } from '../controllers/UserController.js';
+
+const router = express.Router();
+
+//Product Routes
+
+http://localhost:5023/products
+
+router.get("/products", showProducts);
+
+router.get("/products/:id", showProductById);
+
+router.post("/products", createProduct);
+
+router.put("/products/:id", updateProduct);
+
+router.delete("/products/:id", deleteProduct);
+
+router.put("/products/:id/status", updateProductStatusController);
+
+router.put("/products/:id/quantity", updateProductQuantityController);
+
+//Category Routes
+
+router.get("/categories", showCategories);
+
+router.get("/categorys/:id", showCategoryById);
+
+router.post("/categories", createCategory);
+
+router.put("/categorys/:id", updateCategory);
+
+router.delete("/categorys/:id", deleteCategory);
+
+//Users Routes
+
+router.post('/login', loginUser);
+
+router.get("/users", showUsers);
+
+router.post("/login", showUserByUsername);
+
+router.put("/users/:id", updateUser);
+
+router.delete("/users/:id", deleteUser);
+
+router.put("/users/:id/status", updateUserStatusController);
+
+export default router;
